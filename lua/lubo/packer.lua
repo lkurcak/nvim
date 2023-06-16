a671160cf8d9fc5ed99a9c538ccea74c8e8100a4
lubomirkurcak <lubomirkurcak@gmail.com>
2023-06-16 08:36:47 +0200

    changes

--- lua/lubo/packer.lua
+++ lua/lubo/packer.lua
@@ -4,44 +4,56 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use({ 'rose-pine/neovim', as = 'rose-pine', config = function()
-	  vim.cmd('colorscheme rose-pine')
-  end })
-use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    --use({ 'rose-pine/neovim', as = 'rose-pine', config = function()
+    --    vim.cmd('colorscheme rose-pine')
+    --end })
+    use({
+        'bluz71/vim-moonfly-colors',
+        as = 'moonfly',
+        config = function()
+            vim.cmd('colorscheme moonfly')
+        end
+    })
+    --use({ 'rafamadriz/neon', as = 'neon', config = function()
+    --    vim.g.neon_style = 'dark'
+    --    vim.cmd('colorscheme neon')
+    --end })
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-use('theprimeagen/harpoon')
-use('mbbill/undotree')
-use('tpope/vim-fugitive')
-use('github/copilot.vim')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    --use('github/copilot.vim')
+    use('tpope/vim-abolish')
+    use('Raimondi/delimitMate')
 
-use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {                                      -- Optional
-      'williamboman/mason.nvim',
-      run = function()
-        pcall(vim.cmd, 'MasonUpdate')
-      end,
-    },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-  }
-}
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
+    }
 end)
-
-
