d570e9448aa274ee4664fbe01d17e476f8548634
Ľubomír Kurčák <lubomirkurcak@gmail.com>
2024-04-02 03:21:21 +0200

    ignore node modules

--- init.lua
+++ init.lua
@@ -246,6 +246,7 @@ telescope.setup({
     defaults = {
         -- `hidden = true` is not supported in text grep commands.
         vimgrep_arguments = vimgrep_arguments,
+        file_ignore_patterns = { "node_modules" },
     },
     pickers = {
         find_files = {
