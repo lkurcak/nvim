9c9e259428338156d032df88aeabbe5204eb5449
Ľubomír Kurčák <lubomirkurcak@gmail.com>
2025-02-23 15:28:35 +0100

    remove comment

--- init.lua
+++ init.lua
@@ -296,7 +296,6 @@ table.insert(vimgrep_arguments, "!**/*.png")
 
 telescope.setup({
     defaults = {
-        -- `hidden = true` is not supported in text grep commands.
         vimgrep_arguments = vimgrep_arguments,
         file_ignore_patterns = { "node_modules", "target" },
     },
