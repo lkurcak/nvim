35af4dac477f32e5b9e8c5b0678cd85a1d25a169
 Ľubomír Kurčák <lubomirkurcak@Lubomirs-MacBook-Air.local>
2025-09-04 20:46:23 +0200

    oil show hidden files and don't override <C-p>

--- init.lua
+++ init.lua
@@ -292,7 +292,30 @@ vim.keymap.set("n", "<C-k>", function() harpoon_term.gotoTerminal(2) end, { nore
 vim.keymap.set("n", "<C-l>", function() harpoon_term.gotoTerminal(3) end, { noremap = true })
 
 -- Oil
-require("oil").setup()
+require("oil").setup({
+    use_default_keymaps = false,
+    keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        -- ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        -- ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        -- ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },
+    view_options = {
+        show_hidden = true,
+    },
+})
 
 -- Faster
 require('faster').setup()
